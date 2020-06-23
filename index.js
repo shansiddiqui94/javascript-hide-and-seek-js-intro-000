f327ecaa791807df58b4960557f4b097bfa0f6ad
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-06-23 22:36:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function nestedTarget(){
   function deepestChild(){
     return document.querySelector("#grand-node div div div div")
   }
+  
+  function increaseRankBy(n){
+    const rList=document.querySelectorAll(".ranked-list li")
+    for (let i=0;i<rList.length;i++){
+      rList[i].innerHTML=parseInt(rList[i].innerHTML)+n
+    }
