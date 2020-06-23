bae0fd3071cc34c9d841fb32071d443418891f25
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-06-23 22:37:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector("#nested .target")
+  }
+
+  function deepestChild(){
+    return document.querySelector("#grand-node div div div div")
+  }
+
+  function increaseRankBy(n){
+    const rList=document.querySelectorAll(".ranked-list li")
+    for (let i=0;i<rList.length;i++){
+      rList[i].innerHTML=parseInt(rList[i].innerHTML)+n
+    }
+  }
