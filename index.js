71e15f2b6071c1cdbf767561f138115592987dc6
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-06-23 22:23:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector){
   return document.querySelector(selector)
 }
 
-function nestedTarget() {
-return document.querySelector(.target)
+function nestedTarget(){
+  return document.querySelector("#nested .target")
   }
